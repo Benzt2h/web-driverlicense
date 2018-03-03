15255cf67a7430dc22564d49f0e948f73cf1378d
NB <nattapong.wo58@snru.ac.th>
2018-03-04 02:50:51 +0700

    admin-page user manager 100% not link examintion manager

---
+++ add_success.php
@@ -0,0 +1,67 @@
+<?php
+$id=$_POST['id'];
+$pass=$_POST['pass'];
+$name=$_POST['name'];
+
+$serverName = "localhost";
+$userName = "root";
+$userPassword = "";
+$dbName = "driver_license";
+
+ $conn = mysqli_connect($serverName,$userName,$userPassword,$dbName);
+ $sql = "INSERT INTO tb_user (ID,Pass,name) VALUES ('$id','$pass','$name')";  
+ $query = mysqli_query($conn,$sql);
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Admin Page</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/navbar-top.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+      <a class="navbar-brand" href="admin.php">Admin Page</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="admin.php">หน้าแรก<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="admin-user.php">จัดการสมาชิก</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">จัดการข้อสอบ</a>
+          </li>
+        </ul>
+        <form class="form-inline mt-2 mt-md-0" >
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+<div class="container">
+    เพิ่มข้อมูลเรียบร้อยแล้ว
+    <a href="admin-user.php" class="btn btn-success">กลับ</a>
+</div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js" ></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ edit_user.php
@@ -0,0 +1,78 @@
+<?php
+$id = $_GET['id'];
+$serverName = "localhost";
+$userName = "root";
+$userPassword = "";
+$dbName = "driver_license";
+
+ $conn = mysqli_connect($serverName,$userName,$userPassword,$dbName);
+ $sql = "SELECT * FROM tb_user WHERE ID='$id'";
+ $query = mysqli_query($conn,$sql);
+ $result=mysqli_fetch_array($query);
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Admin Page</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/navbar-top.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+      <a class="navbar-brand" href="admin.php">Admin Page</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="admin.php">หน้าแรก<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="admin-user.php">จัดการสมาชิก</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">จัดการข้อสอบ</a>
+          </li>
+        </ul>
+        <form class="form-inline mt-2 mt-md-0" >
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+<div class="container">
+<form action="update_user.php" method="post">
+  <div class="form-group">
+    <label for="id">ID</label>
+    <input type="text" class="form-control" name="id" value="<?php echo $result['ID']?>">
+  </div>
+  <div class="form-group">
+    <label for="pass">Password</label>
+    <input type="text" class="form-control" name="pass" value="<?php echo $result['Pass']?>">
+  </div>
+  <div class="form-group">
+    <label for="name">Name</label>
+    <input type="text" class="form-control" name="name" value="<?php echo $result['name']?>">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js" ></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
