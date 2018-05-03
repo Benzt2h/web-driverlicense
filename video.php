af43be878e98522d223c49af6684ebede1a28c9a
benzt2h-nb <benzt2h@hotmail.com>
2018-05-04 02:09:14 +0700

    add video.php

---
+++ video.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Driver License</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/navbar-top.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+      <a class="navbar-brand" href="index.php">Driver License</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">หน้าแรก<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="video.php">วีดีโออบรมเตรียมก่อนทำข้อสอบพร้อมข้อสอบ</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="user-question.php">ทำข้อสอบ</a>
+          </li>
+        </ul>
+        <?php
+            session_start();
+            if(!isset($_SESSION['User'])){
+        ?>
+        <a href="register.php" class="btn btn-outline-success my-2 my-sm-0">สมัครสมาชิก</a>
+        <a href="login.php" class="btn btn-outline-success my-2 my-sm-0">Login</a>
+        <?php
+            }
+            else {
+        ?>
+        <a href="logout.php" class="btn btn-outline-success my-2 my-sm-0">Logout</a>
+            <?php } ?>
+      </div>
+    </nav>
+
+    <main role="main" class="container">
+      <div class="jumbotron">
+        <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/AizS6ChvF_s"></iframe>
+        </div>
+      </div>
+    </main>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js" ></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
