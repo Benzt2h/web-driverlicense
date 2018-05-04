f749643f35b8843fe3ffb3ba6d97892d48e41589
benzt2h-nb <benzt2h@hotmail.com>
2018-05-04 17:40:52 +0700

    fix bug forget POST var

--- login-ch-admin.php
+++ login-ch-admin.php
@@ -1,11 +1,13 @@
 <?php
 session_start();
  require 'config.php';
+ $user = $_POST['user'];
+ $password = $_POST['password'];
  $sql = "SELECT * FROM admin WHERE User='$user' AND Password='$password'";
  $query = mysqli_query($conn,$sql);
  $result=mysqli_fetch_array($query,MYSQLI_ASSOC);
- if(!$result)
- header("location:login-admin.php");
+ if(!$result){
+ header("location:login-admin.php");}
 else{
     $_SESSION['User']=$result['User'];
     $_SESSION['adminname']=$result['Name'];
